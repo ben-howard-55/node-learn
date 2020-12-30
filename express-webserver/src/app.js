f4f57e11bff4f921655d797cada4052cc0f69c47
Benjamin Howard <bjhoward5555@gmail.com>
2020-12-31 12:22:36 +1300

    added JSON and public folder

--- express-webserver/src/app.js
+++ express-webserver/src/app.js
@@ -1,29 +1,27 @@
 const express = require('express');
+const path = require('path');
+
+// dirname and filename variables:
+console.log(__dirname);
+console.log(__filename);
 
 // entry to server
 const app = express();
 
-// for entry on domain
-app.get('', (req, res) => {
-    res.send('<h1>Welcome</h1>'); // html
-})
+// use path.join() to create a longer path to the index
+let public = path.join(__dirname, '../public')
 
-// for entering a sub page, called help
-app.get('/help', (req, res) => {
-    res.send({ // JSON
-        name: 'Ben',
-        age: '20'
-    });
-})
+// allow express to access files in folder
+app.use(express.static(public))
 
 // about route
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.send('<h1>About</h1>'); //inline html
 })
 
 // weather route
 app.get('/weather', (req, res) => {
-    res.send({
+    res.send({ // send JSON
         forecast: 'raining',
         location: 'auckland'
     });
