c50b7ccd9b4ff57776dbd9af34eb436f22e19966
Benjamin Howard <bjhoward5555@gmail.com>
2020-12-31 11:42:11 +1300

    added express server routes

--- express-webserver/src/app.js
+++ express-webserver/src/app.js
@@ -1,1 +1,35 @@
+const express = require('express');
+
 // entry to server
+const app = express();
+
+// for entry on domain
+app.get('', (req, res) => {
+    res.send('<h1>Welcome</h1>'); // html
+})
+
+// for entering a sub page, called help
+app.get('/help', (req, res) => {
+    res.send({ // JSON
+        name: 'Ben',
+        age: '20'
+    });
+})
+
+// about route
+app.get('/about', (req, res) => {
+    res.send('About Page');
+})
+
+// weather route
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'raining',
+        location: 'auckland'
+    });
+})
+
+// start the server
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
