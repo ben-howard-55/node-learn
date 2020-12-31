1b0d3a714e338930b2c8b9e9231b9f0ad0d9ac1e
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2020-12-31 14:11:35 +1300

    finished setting up express and html for tutorial

--- socketIO/src/index.js
+++ socketIO/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 // init express function app
 var app = express();
@@ -7,9 +8,15 @@ var app = express();
 // create http server, that the express function is passed to
 var server = http.createServer(app);
 
+// use path.join() to create a longer path to the index
+let public = path.join(__dirname, "../public");
+
+// allow express to access files in folder
+app.use(express.static(public));
+
 // define route handler that is called on website home route
 app.get("", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(public + "/index.html"); // send index.html file to home route
 });
 
 // make server listen to port 3000
