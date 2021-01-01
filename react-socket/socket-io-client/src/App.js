ff0a162d2cebbaa31596ab917ea38472c3d14782
Ben Howard <62419637+ben-howard-55@users.noreply.github.com>
2021-01-01 14:55:14 +1300

    added react useEffect and useState logic, for date

--- react-socket/socket-io-client/src/App.js
+++ react-socket/socket-io-client/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import openSocket from "socket.io-client";
+
+// socket is opened on the express server domain,
+// Must include the transports: ["websockets"] in order to
+// not violate CORS, (In this case, the express is on :3000,
+// and the react server on :3001)
+const socket = openSocket("http://localhost:3000", {
+  transports: ["websocket"],
+});
 
 function App() {
+  const [res, setRes] = useState("");
+
+  // on component mount, connect to the socket
+  useEffect(() => {
+    // define socket behaviour
+    socket.on("FromAPI", (msg) => {
+      setRes(msg);
+    });
+
+    // clean up the socket, on component unmount
+    return () => socket.disconnect();
+  }, []);
+
+  // display the result of the socket information
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It's <time dateTime={res}>{res}</time>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- react-socket/socket-io-server/src/app.js
+++ react-socket/socket-io-server/src/app.js
@@ -8,31 +8,36 @@ const index = require("../public/routes/index");
 const app = express();
 // use the index.js express function on the default route
 app.use(index);
-
+// create http server
 const server = http.createServer(app);
-
-const io = socketIo(server); // < Interesting!
-
+// create socket server
+const io = socketIo(server);
 
 let interval;
 
+// logic for connecting socket.
 io.on("connection", (socket) => {
-    console.log("New Client Connected");
-    if (interval) {
-        clearInterval(interval);
-    }
-    interval = setInterval(() => getApiAndEmit(socket), 1000);
-    // on disconnect clear the interval
-    socket.on("disconnect", () => {
-        console.log("Client Disconnected");
-        clearInterval(interval);
-    })
-})
+  console.log("New Client Connected");
+  if (interval) {
+    clearInterval(interval);
+  }
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-    // emitting a new message. Will be consumed by the client
-    socket.emit("FromAPI", response)
-}
+  // set up time out for sending date information. Sends every second.
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
 
+  // on disconnect clear the interval
+  socket.on("disconnect", () => {
+    console.log("Client Disconnected");
+    clearInterval(interval);
+  });
+});
+
+// send a message to be consumed by the clients
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  // emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+// start server
 server.listen(port, () => console.log(`Listening on port ${port}`));
