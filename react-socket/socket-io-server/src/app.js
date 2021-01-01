04613c778185b9d842d5279e93eeae85b17f9350
Benjamin Howard <bjhoward5555@gmail.com>
2021-01-01 13:17:10 +1300

    added server logic for react-socket example

--- react-socket/socket-io-server/src/app.js
+++ react-socket/socket-io-server/src/app.js
@@ -13,4 +13,26 @@ const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-// const getApiAndEmit = "TODO";
+
+let interval;
+
+io.on("connection", (socket) => {
+    console.log("New Client Connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 1000);
+    // on disconnect clear the interval
+    socket.on("disconnect", () => {
+        console.log("Client Disconnected");
+        clearInterval(interval);
+    })
+})
+
+const getApiAndEmit = socket => {
+    const response = new Date();
+    // emitting a new message. Will be consumed by the client
+    socket.emit("FromAPI", response)
+}
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
